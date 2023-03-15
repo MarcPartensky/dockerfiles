2b85bc1c52eeeaabe83b197c0727190203d8dd20
Marc Partensky <marc.partensky@gmail.com>
2023-03-15 17:41:50 +0100

    changed entrypoint ytdlp

--- yt-dlp/entrypoint.sh
+++ yt-dlp/entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-yt-dlp \
-    -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' \
-    -ciw --audio-quality 0 \
-    --embed-thumbnail \
-    -o '%(title)s.%(ext)s' \
-    --rm-cache-dir \
-    $PLAYLIST
+# default value
+PLAYLIST_PATH=${PLAYLIST_PATH:-"/root/downloads/list.txt"}
+
+while read playlist
+do
+    yt-dlp \
+        -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' \
+        -ciw --audio-quality 0 \
+        --embed-thumbnail \
+        -o '%(title)s.%(ext)s' \
+        --rm-cache-dir \
+        $playlist
+done < $PLAYLIST_PATH
